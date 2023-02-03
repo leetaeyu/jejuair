65f6076872b23da971065b8a0bc80e5cedf4924e
leeteayu <ehdrkq2005@naver.com>
2023-02-03 14:55:00 +0900

    23.02.03

--- js/common.js
+++ js/common.js
@@ -1,17 +1,21 @@
 $(function(){
-    $(".button1").click(function(){
+    const moBt = $(".mo_button");
+    const d1b = $(".mo_depth1 > .mo_button");
+
+
+    $(".pc_button1").click(function(){
         $(".pc_depth2_box").removeClass("click");
         $(".depth2_box1").toggleClass("click");
     });
-    $(".button2").click(function(){
+    $(".pc_button2").click(function(){
         $(".pc_depth2_box").removeClass("click");
         $(".depth2_box2").toggleClass("click");
     });
-    $(".button3").click(function(){
+    $(".pc_button3").click(function(){
         $(".pc_depth2_box").removeClass("click");
         $(".depth2_box3").toggleClass("click");
     });
-    $(".button4").click(function(){
+    $(".pc_button4").click(function(){
         $(".pc_depth2_box").removeClass("click");
         $(".depth2_box4").toggleClass("click");
     });
@@ -19,5 +23,18 @@ $(function(){
     $(".pc_depth2_box").mouseleave(function(){
         $(this).removeClass("click");
     });
+
+    $(".gnb_button").click(function(){
+        $("body").toggleClass("hidden");
+        $(".map").toggleClass("active");
+    });
+
+    $(".mo_depth1 > button").click(function(){
+        $(this).parent().siblings().find(".mo_depth2_box").stop().slideUp(500);
+        $(this).next().stop().slideToggle(500);
+        $(this).parent().siblings().removeClass("active");
+        $(this).parent().toggleClass("active");
+    });
+
 
 });
--- js/main.js
+++ js/main.js
@@ -1,8 +1,97 @@
 $(function(){
+    const fpCon = $("#jeju_main_container");
+
+    let ftEl = $(".ft_container").clone();
+    fpCon.append(ftEl);
+
     $("#jeju_main_container").fullpage({
         navigation:true,
         navigationPosition:"left",
-        navigationTooltips:["예약하기", "공백", "공백", "공백", "공백"],
-        anchors: ["what", "where", "how", "bbs",],
+        navigationTooltips:["예약하기", "추천항공권", "소식", "안내","푸터"],
+        anchors: ["rsv", "price", "news", "info", "ft"],
+    });
+
+    var priceSwiper = new Swiper(".price_swiper", {
+        slidesPerView: 1,
+        spaceBetween:40,
+        speed:800,
+        // centeredSlides: true,ㄴ
+        loop:true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".next",
+            prevEl: ".prev",
+          },
+        breakpoints: {
+          700: {
+            slidesPerView: 2,  //브라우저가 768보다 클 때
+            spaceBetween: 40,
+              
+          },
+
+          1024: {
+            slidesPerView: 3,  //브라우저가 1278보다 클 때
+            spaceBetween: 20,
+          },
+        },
+    });
+
+    var newsSwiper = new Swiper(".news_swiper", {
+        slidesPerView: 1,
+        // spaceBetween:60,
+        speed:800,
+        // centeredSlides: true,
+        loop:true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".next",
+            prevEl: ".prev",
+          },
+        breakpoints: {
+          700: {
+            slidesPerView: 2,  //브라우저가 768보다 클 때
+            spaceBetween: 40,
+              
+          },
+
+          1024: {
+            slidesPerView: 4,  //브라우저가 1278보다 클 때
+            spaceBetween: 20,
+          },
+        },
+    });
+
+    var infoSwiper = new Swiper(".info_swiper", {
+        slidesPerView: 1,
+        // spaceBetween:60,
+        speed:800,
+        // centeredSlides: true,
+        loop:true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".next",
+            prevEl: ".prev",
+          },
+        breakpoints: {
+          700: {
+            slidesPerView: 3,  //브라우저가 768보다 클 때
+            spaceBetween: 40,
+              
+          },
+
+        //   1024: {
+        //     slidesPerView: 4,  //브라우저가 1278보다 클 때
+        //     spaceBetween: 20,
+        //   },
+        },
     });
 })
